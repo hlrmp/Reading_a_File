dd1a1d2991258970db7affe011c8ad3aedd14038
hlrmp <haysjhune@gmail.com>
2023-11-13 11:44:23 +0800

    Add project files.

---
+++ Reading_a_File/FrmOpenTextFile.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Reading_a_File
+{
+    partial class FrmOpenTextFile
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvShowText = new System.Windows.Forms.ListView();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // lvShowText
+            // 
+            this.lvShowText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lvShowText.HideSelection = false;
+            this.lvShowText.Location = new System.Drawing.Point(101, 38);
+            this.lvShowText.Name = "lvShowText";
+            this.lvShowText.Size = new System.Drawing.Size(487, 227);
+            this.lvShowText.TabIndex = 0;
+            this.lvShowText.UseCompatibleStateImageBehavior = false;
+            this.lvShowText.View = System.Windows.Forms.View.List;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.BackColor = System.Drawing.Color.Gray;
+            this.btnOpen.FlatAppearance.BorderColor = System.Drawing.Color.Red;
+            this.btnOpen.FlatAppearance.BorderSize = 3;
+            this.btnOpen.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnOpen.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Red;
+            this.btnOpen.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOpen.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnOpen.Location = new System.Drawing.Point(279, 285);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(139, 56);
+            this.btnOpen.TabIndex = 1;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = false;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // FrmOpenTextFile
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
+            this.ClientSize = new System.Drawing.Size(680, 393);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.lvShowText);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "FrmOpenTextFile";
+            this.Text = "FrmOpenTextFile";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvShowText;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
